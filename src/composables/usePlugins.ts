66cf767b2981cb4d4c24a95647c68fbaf40561bd
mktcode <kontakt@markus-kottlaender.de>
2021-11-26 19:45:20 +0100

    moved functions to global scope

--- src/composables/usePlugins.ts
+++ src/composables/usePlugins.ts
@@ -1,21 +1,21 @@
 import { defineAsyncComponent } from 'vue';
 import plugins from '@/plugins/_plugins.json';
 
+const usePlugin = async (plugin: string) =>
+  (await import(`../plugins/${plugin}/index.ts`)).default();
+
+
+const getComponent = (plugin: string, component: string) => {
+  return defineAsyncComponent(() =>
+    import(`../plugins/${plugin}/components/${component}.vue`)
+  );
+}
+
+const getInfo = (plugin) => {
+  return plugins[plugin];
+}
+
 export function usePlugins() {
-  const usePlugin = async (plugin: string) =>
-    (await import(`../plugins/${plugin}/index.ts`)).default();
-
-
-  const getComponent = (plugin: string, component: string) => {
-    return defineAsyncComponent(() =>
-      import(`../plugins/${plugin}/components/${component}.vue`)
-    );
-  }
-
-  const getInfo = (plugin) => {
-    return plugins[plugin];
-  }
-
   return {
     plugins,
     usePlugin,
